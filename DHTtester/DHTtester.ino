cb4ce873d8a72e558c9db8b4639fe079829d73df
fangmike <fangmike23@gmail.com>
2024-06-11 15:42:08 +0300

    .

--- DHTtester/DHTtester.ino
+++ DHTtester/DHTtester.ino
@@ -34,7 +34,7 @@ DHT dht(DHTPIN, DHTTYPE);
 int light_val;
 
 void setup() {
-  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
   Serial.begin(9600);
   Serial.println(F("DHTxx test!"));
 
@@ -46,11 +46,11 @@ void loop() {
   delay(500);
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
+  float h = 0;
   // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
+  float t = 0;
   // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+  float f = 0;
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
--- env_moisture_sensors/env_moisture_sensors.ino
+++ env_moisture_sensors/env_moisture_sensors.ino
@@ -62,7 +62,7 @@ void loop() {
     digitalWrite(LED_PIN, HIGH);
   } 
   else {
-    digitalWRITE(LED_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
   }
 
   //calculate average values for moisture probe
